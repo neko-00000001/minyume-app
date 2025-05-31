012d7aaa31f931a08e5a3c6f5fa52e4a357c9341
neko-00000001 <oshima.61.le@gmail.com>
2025-05-31 10:13:45 +0900

    Initial commit

---
+++ src/main/java/com/example/minyumeapp/MinyumeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.minyumeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinyumeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinyumeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minyume_app/MinyumeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minyume_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinyumeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
