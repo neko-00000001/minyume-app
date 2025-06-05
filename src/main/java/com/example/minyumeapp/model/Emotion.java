a48f13f888ecd2d3d01c2c51bd18beb1867f7eaa
ねこ <oshima.61.le@gmail.com>
2025-06-05 16:24:12 +0900

    Reduce dream emotion options to kidoairaku

--- src/main/java/com/example/minyumeapp/model/Emotion.java
+++ src/main/java/com/example/minyumeapp/model/Emotion.java
@@ -1,10 +1,8 @@
 package com.example.minyumeapp.model;
 
 public enum Emotion {
-    HAPPY,
-    SAD,
-    SCARY,
-    FUNNY,
-    STRANGE,
-    PEACEFUL
+    JOY,
+    ANGER,
+    SORROW,
+    PLEASURE
 }
